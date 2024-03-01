907259e13338ba2bf4312c5263801ccc523c3093
Dat Tran <datttrian@gmail.com>
2024-03-01 00:42:45 +0000

    Add a moderation layer

--- handlers.py
+++ handlers.py
@@ -1,39 +1,34 @@
 import openai
 from dotenv import load_dotenv
-from colorama import Fore
+import os
+
 
 load_dotenv()
 client = openai.OpenAI()
 
 # Constants
-PERSONA = "You are a skilled stand-up comedian with a quick wit and charismatic presence, known for their clever storytelling and ability to connect with diverse audiences through humor that is both insightful and relatable."
 MODEL_ENGINE = "gpt-3.5-turbo"
-MESSAGE_SYSTEM = " You are a skilled stand-up comedian with a knack for telling 1-2 sentence funny stories."
+MESSAGE_SYSTEM = "You are a helpful assistant"
 messages = [{"role": "system", "content": MESSAGE_SYSTEM}]
 
 
-def to_dict(obj):
-    return {
-        "content": obj.content,
-        "role": obj.role,
-    }
+def moderate(user_input):
+    response = client.moderations.create(input=user_input)
+    return response.results[0].flagged
 
 
-def print_messages(messages):
-    messages = [message for message in messages if message["role"] != "system"]
-    for message in messages:
-        role = "Bot" if message["role"] == "assistant" else "You"
-        print(Fore.BLUE + role + ": " + message["content"])
-    return messages
-
-
-def generate_chat_completion(user_input=""):
-    messages.append({"role": "user", "content": user_input})
+def generate_chat_completion(user_input, messages):
+    flagged = moderate(user_input)
+    print(f"Flagged: {flagged}")
+    if flagged:
+        return ":red[Your comment has been flagged as inappropriate.]"
     completion = client.chat.completions.create(
-        model="gpt-3.5-turbo",
+        model=MODEL_ENGINE,
         messages=messages,
+        temperature=0,
+        max_tokens=60,
+        top_p=1.0,
+        frequency_penalty=0.0,
+        presence_penalty=0.0,
     )
-    message = completion.choices[0].message
-    messages.append(to_dict(message))
-    print_messages(messages)
-    return message.content
+    return completion.choices[0].message.content
