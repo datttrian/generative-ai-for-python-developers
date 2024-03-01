d36f7dee4b772768992a27ca36b59be665e6c308
Dat Tran <datttrian@gmail.com>
2024-03-01 10:07:03 +0000

    Whisper Audio API: Speech-to-text

--- main.py
+++ main.py
@@ -1,51 +1,46 @@
 import streamlit as st
-from handlers import downloadFile, generate_image, get_files
+import tempfile
+import openai
+import os
+from dotenv import load_dotenv
+from utils import speech_to_text
 
-desired_size = (600, 400)
+# Load environment variables
+load_dotenv()
+
+# Set OpenAI API key
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 # Streamlit App
-st.title("🖼️ Image Generation Gallery ✨")  # Add a title
-margin = '<div style="margin: 20px 5px;"></div>'
+st.title("Audio transcription")  # Add a title
+
+# Custom style for blue button
+st.markdown(
+    """
+    <style>
+        .stButton>button {
+            background-color: transparent;
+            border: 1px solid #3498db;
+            float: right;
+        }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
 
 # User input
 with st.form("user_form", clear_on_submit=True):
-    user_input = st.text_input("Type something")
-    submit_button = st.form_submit_button(label="Send")
+    uploaded_file = st.file_uploader("Choose a file")
+    submit_button = st.form_submit_button(label="Submit")
 
-# Press Enter to generate response from chatbot
-if submit_button:
-    with st.spinner("Generating image..."):
-        image = generate_image(user_input)
-        st.image(image, use_column_width=True)
-        saved_image = downloadFile(user_input, image)
-        st.success("Image successfully generated and saved to media folder")
+# Process the uploaded file
 
-
-def display_gallery():
-    """Display all images in the gallery"""
-    images = get_files()
-    length = len(images)
-    st.markdown(margin, unsafe_allow_html=True)
-
-    for i in range(0, length, 2):
-        with st.container():
-            col1, col2 = st.columns(2)
-
-            with col1:
-                if i < length:
-                    image_resized = images[i]["file"].resize((600, 400))
-                    st.image(
-                        image_resized,
-                        caption=images[i]["title"],
-                    )
-            with col2:
-                if i + 1 < length:
-                    image_resized = images[i + 1]["file"].resize((600, 400))
-                    st.image(
-                        image_resized,
-                        caption=images[i + 1]["title"],
-                    )
-
-
-if __name__ == "__main__":
-    display_gallery()
+if submit_button and uploaded_file is not None:
+    with st.spinner("Transcribing..."):
+        # Save the uploaded file to a temporary file
+        with tempfile.NamedTemporaryFile(
+            delete=False, suffix=os.path.splitext(uploaded_file.name)[1]
+        ) as temp_file:
+            temp_file.write(uploaded_file.getvalue())
+            temp_file_path = temp_file.name
+            pass
