df2b9c8deceda56e871882816902b2c1cdce222d
Dat Tran <datttrian@gmail.com>
2024-03-01 15:27:48 +0000

    Create a vector store and embeddings (Chroma)

--- main.py
+++ main.py
@@ -9,13 +9,16 @@ from langchain.prompts.chat import (
 )
 from langchain.schema import StrOutputParser
 
+from langchain.embeddings.openai import OpenAIEmbeddings
+
+
 from langchain.document_loaders import TextLoader
-from langchain.embeddings.sentence_transformer import SentenceTransformerEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import Chroma
 
 load_dotenv()
 
+embeddings = OpenAIEmbeddings()
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 LANGUAGE_MODEL = "gpt-3.5-turbo-instruct"
@@ -34,24 +37,33 @@ chat_prompt = ChatPromptTemplate.from_messages(
     [system_message_prompt, human_message_prompt]
 )
 
-# load the document and split it into chunks
-loader = TextLoader("./docs/faq.txt")
-documents = loader.load()
-
-# split it into chunks
-text_splitter = CharacterTextSplitter(chunk_size=100, chunk_overlap=0)
-docs = text_splitter.split_documents(documents)
-
-print(docs)
-
 
 def main():
-    user_input = "Do you ship to Europe?"
+    user_query = "Do you ship to Europe?"
 
     # LCEL makes it easy to build complex chains from basic components, and supports out of the box functionality such as streaming, parallelism, and logging.
     chain = chat_prompt | model | str_parser
     # message = chain.invoke({"question": user_input})
     # print(message)
+    # load the document and split it into chunks
+
+    loader = TextLoader("./docs/faq.txt")
+    documents = loader.load()
+
+    # split it into chunks
+    text_splitter = CharacterTextSplitter(chunk_size=100, chunk_overlap=0)
+    documents = text_splitter.split_documents(documents)
+
+    # load documents to the vector store
+    # load it into Chroma
+
+    db = Chroma.from_documents(documents, embeddings)
+
+    # query it
+    docs = db.similarity_search(user_query)
+
+    # print results
+    print(docs[0].page_content)
 
 
 if __name__ == "__main__":
